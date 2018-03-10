b66202edeb04f01995b420e710782792a7611d0c
barsandcat <barsandcat@gmail.com>
2018-03-10 21:02:44 +0200

    Tutorial 2 wip

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,27 +1,47 @@
 package com.example.examplemod;
 
+import com.example.examplemod.proxy.CommonProxy;
+
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 @Mod(modid = Ref.MODID, name = Ref.NAME, version = Ref.VERSION)
 public class ExampleMod
 {
-    private static Logger logger;
+    @SidedProxy(clientSide = Ref.CLIENT_PROXY, serverSide = Ref.COMMON_RPOXY)
+    public static CommonProxy mProxy;
+
+    private static Logger mLogger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+        mLogger = event.getModLog();
+        mLogger.info("Pre init {}", event);
+        mProxy.PreInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        mLogger.info("Init {}", event);
+        mLogger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        mProxy.Init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        mLogger.info("Post init {}", event);
+        mProxy.PostInit(event);
     }
 }
--- src/main/java/com/example/examplemod/Ref.java
+++ src/main/java/com/example/examplemod/Ref.java
@@ -5,4 +5,6 @@ public class Ref
     public static final String MODID = "examplemod";
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
+    public static final String CLIENT_PROXY = "com.example.examplemod.proxy.ClientProxy";
+    public static final String COMMON_RPOXY = "com.example.examplemod.proxy.CommonProxy";
 }
